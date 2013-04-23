4e71834287f224950115f255645e70bfd5e1889c
jokergoo <jokergoo@gmail.com>
2013-04-23 22:23:32 +0200

    add example of dartboard

--- example/galary.R
+++ example/galary.R
@@ -1,8 +1,8 @@
 
 
-source("R/global.R")
-source("R/plot.R")
-source("R/utils.R")
+source("../R/global.R")
+source("../R/plot.R")
+source("../R/utils.R")
 # library(circlize)
 
 
@@ -146,3 +146,21 @@ arrows(0, 0, 0, 0.7)
 arrows(0, 0, 0.4, 0)
 
 circos.clear()
+
+##################################################################################
+# 
+##################################################################################
+
+
+factors = 1:20
+par(mar = c(1, 1, 1, 1))
+circos.par("gap.degree" = 0, "cell.padding" = c(0, 0, 0, 0), start.degree = 360/40, track.margin = c(0, 0))
+circos.initialize(factors = factors, xlim = c(0, 1))
+circos.trackPlotRegion(ylim = c(0, 1), factors = factors, bg.col = "black", track.height = 0.15)
+circos.trackText(rep(0.5, 20), rep(0.5, 20), labels = c(13, 4, 18, 1, 20, 5, 12, 9, 14, 11, 8, 16, 7, 19, 3, 17, 2, 15, 10, 6), factors = factors, col = "#EEEEEE", font = 2, direction = "horizontal")
+circos.trackPlotRegion(ylim = c(0, 1), factors = factors, bg.col = rep(c("#E41A1C", "#4DAF4A"), 10), bg.border = "#EEEEEE", track.height = 0.05)
+circos.trackPlotRegion(ylim = c(0, 1), factors = factors, bg.col = rep(c("black", "white"), 10), bg.border = "#EEEEEE", track.height = 0.275)
+circos.trackPlotRegion(ylim = c(0, 1), factors = factors, bg.col = rep(c("#E41A1C", "#4DAF4A"), 10), bg.border = "#EEEEEE", track.height = 0.05)
+circos.trackPlotRegion(ylim = c(0, 1), factors = factors, bg.col = rep(c("black", "white"), 10), bg.border = "#EEEEEE", track.height = 0.375)
+draw.sector(x = 0, y = 0, start = 0, end = 360, radius = 0.1, col = "#4DAF4A", border = "#EEEEEE")
+draw.sector(x = 0, y = 0, start = 0, end = 360, radius = 0.05, col = "#E41A1C", border = "#EEEEEE")

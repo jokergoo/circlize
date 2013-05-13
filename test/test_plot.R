9d7e39b3eec2339c9c074fd1e7011f396a0fa863
dota rookie <jokergoo@gmail.com>
2013-05-14 01:46:25 +0300

    Update test_plot.R

--- test/test_plot.R
+++ test/test_plot.R
@@ -1,4 +1,3 @@
-
 source("R/global.R")
 source("R/plot.R")
 source("R/utils.R")
@@ -18,14 +17,33 @@ library(circlize)
 par(mar = c(1, 1, 1, 1))
 factors = letters[1:2]
 for (theta in seq(-360, 360, by = 30)) {
-	circos.par(start.degree = theta)
+	circos.par(start.degree = theta, cell.padding=c(0,0,0,0))
 	circos.initialize(factors = factors, xlim = c(0, 1))
 	circos.trackPlotRegion(ylim = c(0, 1), panel.fun = function(x, y) {
 		y = runif(20)
 		circos.lines(1:20/20, y, col = "red")
+                circos.lines(c(0,1), c(0,0), col="green")
 		
 		y = runif(20)
 		circos.lines(20:1/20, y, col = "blue")
+                circos.lines(c(1,0), c(0,0), col="yellow")
+	})
+	show.index()
+	circos.clear()
+	Sys.sleep(1)
+}
+
+for (theta in seq(-360, 360, by = 30)) {
+	circos.par(start.degree = theta, cell.padding=c(0,0,0,0), clock.wise=FALSE)
+	circos.initialize(factors = factors, xlim = c(0, 1))
+	circos.trackPlotRegion(ylim = c(0, 1), panel.fun = function(x, y) {
+		y = runif(20)
+		circos.lines(1:20/20, y, col = "red")
+                circos.lines(c(0,1), c(0,0), col="green")
+		
+		y = runif(20)
+		circos.lines(20:1/20, y, col = "blue")
+                circos.lines(c(1,0), c(0,0), col="yellow")
 	})
 	show.index()
 	circos.clear()

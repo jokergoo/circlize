bb5bc42acc2b7896c0e5761b859d4b15caecf511
jokergoo <jokergoo@gmail.com>
2013-05-14 22:01:46 +0200

    vignette improved

--- test/test_plot.R
+++ test/test_plot.R
@@ -141,3 +141,13 @@ circos.trackPlotRegion(ylim = c(0, 1), factors = factors, panel.fun = function(x
 	circos.rect(0, 0, 1, 1, col = "green")
 })
 circos.clear()
+
+################################################################
+
+factors = 1:4
+
+par(mar = c(1, 1, 1, 1))
+circos.initialize(factors = factors, xlim = c(0, 1))
+circos.trackPlotRegion(ylim = c(0, 5), factors = sample(1:4, 100, replace = TRUE), x = runif(100), y = runif(100), panel.fun = function(x, y) {
+	circos.points(x, y)
+})

ab562516270d4c782623009fca941b0a0b0fee85
jokergoo <jokergoo@gmail.com>
2013-06-05 21:29:37 +0200

    add test for  in

--- test/01.test_circos.par.R
+++ test/01.test_circos.par.R
@@ -18,6 +18,10 @@ circos.par("cell.padding")
 circos.par("gap.degree" = -10)
 circos.par("gap.degree" = 380)
 
+circos.par("gap.degree" = c(1, 2, 3, 4 ,5))
+get(".CIRCOS.PAR", envir = .CIRCOS.ENV)
+circos.par("gap.degree")
+
 circos.par("aa")
 circos.par("aa" = 1)
 circos.par("gap.degree" = 20, "aa" = 1)
@@ -68,4 +72,59 @@ circos.par("cell.padding" = c(0.8, 0.8, 0.8, 0.8))
 circos.trackPlotRegion(ylim = c(0, 1), factors = factors, panel.fun = function(x, y) {
 	circos.rect(0, 0, 1, 1, col = "green")
 })
+circos.clear()
+
+#############################
+# gap.degree
+factors = 1:8
+
+par(mar = c(1, 1, 1, 1))
+circos.par(gap.degree = 5)
+circos.initialize(factors = factors, xlim = c(0, 1))
+circos.trackPlotRegion(ylim = c(0, 1), factors = factors)
+circos.clear()
+
+circos.par(gap.degree = rep(5, 2))
+circos.initialize(factors = factors, xlim = c(0, 1))
+circos.trackPlotRegion(ylim = c(0, 1), factors = factors)
+circos.clear()
+
+circos.par(gap.degree = rep(5, 10))
+circos.initialize(factors = factors, xlim = c(0, 1))
+circos.trackPlotRegion(ylim = c(0, 1), factors = factors)
+circos.clear()
+
+circos.par(gap.degree = rep(5, 8))
+circos.initialize(factors = factors, xlim = c(0, 1))
+circos.trackPlotRegion(ylim = c(0, 1), factors = factors)
+circos.clear()
+
+circos.par(gap.degree = 1:8*2)
+circos.initialize(factors = factors, xlim = c(0, 1))
+circos.trackPlotRegion(ylim = c(0, 1), factors = factors)
+circos.clear()
+
+circos.par(gap.degree = 1:8*2, clock.wise = FALSE)
+circos.initialize(factors = factors, xlim = c(0, 1))
+circos.trackPlotRegion(ylim = c(0, 1), factors = factors)
+circos.clear()
+
+circos.par(gap.degree = sample(1:8, 8)*2)
+circos.initialize(factors = factors, xlim = c(0, 1))
+circos.trackPlotRegion(ylim = c(0, 1), factors = factors)
+circos.clear()
+
+circos.par(gap.degree = sample(1:8, 8)*2, start.degree = 90)
+circos.initialize(factors = factors, xlim = c(0, 1))
+circos.trackPlotRegion(ylim = c(0, 1), factors = factors)
+circos.clear()
+
+circos.par(gap.degree = sample(1:8, 8)*2, clock.wise = FALSE)
+circos.initialize(factors = factors, xlim = c(0, 1))
+circos.trackPlotRegion(ylim = c(0, 1), factors = factors)
+circos.clear()
+
+circos.par(gap.degree = sample(1:8, 8)*2, start.degree = 90, clock.wise = FALSE)
+circos.initialize(factors = factors, xlim = c(0, 1))
+circos.trackPlotRegion(ylim = c(0, 1), factors = factors)
 circos.clear()

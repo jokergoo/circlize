3e47359f1d28085c3a07286590bc8c76f0d60f9a
jokergoo <jokergoo@gmail.com>
2014-09-18 18:54:07 +0200

    set lend and ljoin to default

--- check.bat
+++ check.bat
@@ -4,5 +4,5 @@ cd ..
 
 R CMD REMOVE circlize
 R CMD build --compact-vignettes=gs+qpdf circlize
-R CMD check --as-cran --timings circlize_0.1.1.tar.gz
-R CMD INSTALL circlize_0.1.1.tar.gz
+R CMD check --as-cran --timings circlize_0.1.2.tar.gz
+R CMD INSTALL circlize_0.1.2.tar.gz

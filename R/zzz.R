a8db968c5de950c72d607d5668bb6c7736a3b5e3
Zuguang Gu <jokergoo@gmail.com>
2017-09-27 21:53:50 +0200

    add startup message

--- R/zzz.R
+++ R/zzz.R
@@ -6,6 +6,6 @@ setLoadActions(function(ns) {
 	msg = paste0("circlize ", utils::packageDescription("dendextend")$Version, "\n",
 		"Documentation: http://jokergoo.github.io/circlize_book/book\n",
 		"Examples: http://jokergoo.github.io/circlize\n",
-		"Citation: Gu Z, et al., circlize implements and enhances circular visualization in R, Bioinformatics 2014.")
+		"Citation: Gu Z, circlize implements and enhances circular visualization in R, Bioinformatics 2014.")
 	packageStartupMessage(msg)
 }

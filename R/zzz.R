9216f4931729eac0269ec85dd181352c935783fa
Zuguang Gu <jokergoo@gmail.com>
2017-09-27 21:55:57 +0200

    remove startup message

--- R/zzz.R
+++ R/zzz.R
@@ -2,10 +2,3 @@ setLoadActions(function(ns) {
 	circos.par("__tempdir__" = tempdir())
 })
 
-.onAttach = function(lib, pkg, ...) {
-	msg = paste0("circlize ", utils::packageDescription("circlize")$Version, "\n",
-		"Documentation: http://jokergoo.github.io/circlize_book/book\n",
-		"Examples: http://jokergoo.github.io/circlize\n",
-		"Citation: Gu Z, circlize implements and enhances circular visualization in R, Bioinformatics 2014.")
-	packageStartupMessage(msg)
-}

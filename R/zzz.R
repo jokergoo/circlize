5bb1d656a430056fab8e29cb7ef60691e1c7735e
Zuguang Gu <jokergoo@gmail.com>
2017-10-31 12:39:45 +0100

    add startup message

--- R/zzz.R
+++ R/zzz.R
@@ -2,3 +2,19 @@ setLoadActions(function(ns) {
 	circos.par("__tempdir__" = tempdir())
 })
 
+.onAttach = function(libname, pkgname) {
+    version = packageDescription(pkgname, fields = "Version")
+
+  	msg = paste0("========================================
+", pkgname, " version ", version, "
+CRAN page: https://cran.r-project.org/package=circlize
+Github page: https://github.com/jokergoo/circlize
+Documentation: http://jokergoo.github.io/circlize_book/book/
+
+If you use it in published research, please cite:
+Gu, Z. circlize implements and enhances circular visualization 
+  in R. Bioinformatics 2014.
+========================================
+")
+    packageStartupMessage(msg)
+}
